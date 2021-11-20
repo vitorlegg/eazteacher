3d770dcd0303c3b87c9f7d2a1a8d2a05eb922806
vitorlegg <vitorleggieri@hotmail.com>
2021-11-19 22:06:39 -0300

    Removing Types

--- server/config/multer.js
+++ server/config/multer.js
@@ -2,26 +2,6 @@ var aws = require('aws-sdk');
 var multer = require('multer');
 var multerS3 = require('multer-s3');
 
-
-
-var SaveType = { 
-    local:{
-        dest: './' ,
-        storage: multer.diskStorage({
-            destination: function (req, file, cb) {
-            cb(null, './temp')
-            },
-            filename: function (req, file, cb) {
-            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-            cb(null, file.fieldname + '-' + uniqueSuffix)
-            }
-        })
-    },
-    s3: 
-}
-
-console.log(SaveType['s3']);
-
 module.exports = multer({
     storage: multerS3({
         s3: new aws.S3(),
